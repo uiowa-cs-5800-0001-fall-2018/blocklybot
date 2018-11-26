a96cce9a0588b7afbb426b49b056fc88d0cd852c
Harley Waldstein <harley@waldstein.io>
2018-11-26 12:45:55 -0600

    Add Diagnostics controller

--- BlockBot.Web/Program.cs
+++ BlockBot.Web/Program.cs
@@ -17,12 +17,12 @@ namespace BlockBot.Web
         {
             string configurationPrefix = "FundChat_";
 
-            Dictionary<string, string> configDictionary = FetchContainerConfiguration(configurationPrefix);
+            IConfigurationRoot ebsConfig = FetchElasticBeanstalkConfiguration(configurationPrefix);
 
             return WebHost.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
-                    config.AddInMemoryCollection(configDictionary);
+                    config.AddConfiguration(ebsConfig);
 
                     // import environment variables with the given prefix
                     config.AddEnvironmentVariables(configurationPrefix);
@@ -43,8 +43,9 @@ namespace BlockBot.Web
         /// <remarks>
         ///     https://stackoverflow.com/a/43221055
         /// </remarks>
-        private static Dictionary<string, string> FetchContainerConfiguration(string configurationPrefix)
+        private static IConfigurationRoot FetchElasticBeanstalkConfiguration(string configurationPrefix)
         {
+            string connectionStringPrefix = "ConnectionStrings:";
             IConfigurationBuilder elasticBeanstalkConfigBuilder = new ConfigurationBuilder();
 
             elasticBeanstalkConfigBuilder.AddJsonFile(
@@ -59,18 +60,42 @@ namespace BlockBot.Web
                 .GetSection("iis:env")
                 .GetChildren()
                 .Select(pair => pair.Value.Split(new[] {'='}, 2))
-                .ToDictionary(keyPair => keyPair[0], keyPair => keyPair[1]);
+                .ToDictionary(keyPair => keyPair[0].Replace("__", ":"), keyPair => keyPair[1]);
 
             Dictionary<string, string> configDictionary = new Dictionary<string, string>();
+            Dictionary<string, string> connectionStringDictionary = new Dictionary<string, string>();
             foreach (KeyValuePair<string, string> keyValuePair in ebConfig)
             {
-                configDictionary.Add(
-                    keyValuePair.Key.StartsWith(configurationPrefix)
-                        ? keyValuePair.Key.Substring(configurationPrefix.Length)
-                        : keyValuePair.Key, keyValuePair.Value);
+                string key = keyValuePair.Key;
+
+                if (key.StartsWith(configurationPrefix))
+                {
+                    key = key.Substring(configurationPrefix.Length);
+                }
+
+                if (key.StartsWith(connectionStringPrefix))
+                {
+                    key = key.Substring(connectionStringPrefix.Length);
+
+                    connectionStringDictionary.Add(key, keyValuePair.Value);
+                }
+                else
+                {
+                    configDictionary.Add(key, keyValuePair.Value);
+                }
             }
 
-            return configDictionary;
+            IConfigurationBuilder outBuilder = new ConfigurationBuilder();
+            outBuilder.AddInMemoryCollection(configDictionary);
+
+            IConfigurationRoot configurationRoot = outBuilder.Build();
+
+            foreach (KeyValuePair<string, string> kvp in connectionStringDictionary)
+            {
+                configurationRoot.GetSection("ConnectionStrings")[kvp.Key] = kvp.Value;
+            }
+
+            return configurationRoot;
         }
     }
 }
