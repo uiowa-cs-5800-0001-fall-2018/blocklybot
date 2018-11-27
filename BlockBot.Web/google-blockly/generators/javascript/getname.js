f56c287d836e8582c90f7f293e3ee4d130968546
Rae Corrigan <rae-corrigan@uiowa.edu>
2018-11-26 18:08:16 -0600

    JS Generators for "Get name" blocks
    Generators for the getname and getnametext blocks.

---
+++ BlockBot.Web/google-blockly/generators/javascript/getname.js
@@ -0,0 +1,10 @@
+﻿
+Blockly.JavaScript['name_block'] = function (block) {
+    var text_question = block.getFieldValue('question');
+    var value_if = Blockly.JavaScript.valueToCode(block, 'if', Blockly.JavaScript.ORDER_ATOMIC);
+    var statements_then = Blockly.JavaScript.statementToCode(block, 'then');
+    var statements_else = Blockly.JavaScript.statementToCode(block, 'else');
+    // TODO: Assemble JavaScript into code variable.
+    var code = '...;\n';
+    return code;
+};
---
+++ BlockBot.Web/google-blockly/generators/javascript/getnametext.js
@@ -0,0 +1,9 @@
+﻿Blockly.JavaScript['name_text_block'] = function (block) {
+    var text_message_text = block.getFieldValue('message_text');
+    var value_if = Blockly.JavaScript.valueToCode(block, 'if', Blockly.JavaScript.ORDER_ATOMIC);
+    var statements_then = Blockly.JavaScript.statementToCode(block, 'then');
+    var statements_else = Blockly.JavaScript.statementToCode(block, 'else');
+    // TODO: Assemble JavaScript into code variable.
+    var code = '...;\n';
+    return code;
+};
